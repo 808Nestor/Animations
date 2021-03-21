355dbb9a5c2f70f1a1b3f3d43b57ad32ecadf28b
ntrillo <nospam@avaniu.com>
2021-03-21 13:53:52 -1000

    basic animation stuff
    animoto!

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+// VARS
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        
+         VStack{
+           
+            Button(action: {
+                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)){
+                    self.animationAmount += 360
+                }
+            }, label: {
+                Text("Click me!")
+            })
+            .padding(50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(
+                .degrees(animationAmount),
+                axis: (x: 1.0, y: 1.0, z: 1.0)
+                )
+        }
+        
     }
 }
 
