05324c08d01c6d00a406b00178ba02aa4df118a1
ntrillo <nospam@avaniu.com>
2021-03-21 11:23:37 -1000

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Nestor Trillo on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
